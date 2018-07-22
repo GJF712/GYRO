11711ee8de619597597767d1cf0e659ed962310a
GJF712 <763314235@qq.com>
2018-07-22 18:34:45 +0800

    //[20180722] v 初版提交

---
+++ BSP/bsp.c
@@ -0,0 +1,29 @@
+/*
+ * bsp.c
+ *
+ *  Created on: 2018Äê6ÔÂ26ÈÕ
+ *      Author: Junfeng.Guo
+ */
+
+#include "bsp.h"
+#include "Timer.h"
+#include "led.h"
+#include "Usart1.h"
+
+
+void Bsp_Init(void){
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | RCC_AHBPeriph_GPIOF, ENABLE);
+	LED_Init();
+
+    TIM16_PWM();
+    TIM17_PWM();
+
+	TIM3_Config();
+	TIM3_NVIC_Config(0);
+
+	TIM14_Config();
+	TIM14_NVIC_Config(1);
+
+	USART1_Init();
+	USART1_NVIC_Config(2);
+}
